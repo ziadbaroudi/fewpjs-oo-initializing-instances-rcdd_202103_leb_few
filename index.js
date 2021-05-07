a0abab3be13dd5112eb9383860ae6acf02e048fd
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-05-07 00:17:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,12 @@ class Lunch{
     this.drink=drink;
   }
 }
+
+class Dinner{
+  constructor(salad,soup,entree,dessert){
+    this.salad=salad;
+    this.soup=soup;
+    this.entree=entree;
+    this._dessert= dessert 
+  }
+}
